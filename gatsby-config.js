7c8eb2ba204555f884b708320b93fed96c89fd75
Jakub Chmielewski <jakubchmielewski80@gmail.com>
2021-10-19 13:14:34 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "gatsby-boilerplate-rendpro-typescript",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-datocms",
+      options: {
+        apiToken: "",
+      },
+    },
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-image",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "",
+      },
+    },
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
